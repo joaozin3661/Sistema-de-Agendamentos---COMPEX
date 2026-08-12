ba8fe9bbe104bc6306acdc617b36ad45fa553bd5
ivandraws <62605967+ivandraws@users.noreply.github.com>
2026-08-11 21:17:11 -0300

    feat!: Adicionado controle CRUD do cliente

---
+++ src/main/java/com/sistemaagendamento/compex/service/ClienteService.java
@@ -0,0 +1,48 @@
+package com.sistemaagendamento.compex.service;
+
+import com.sistemaagendamento.compex.model.Cliente;
+import com.sistemaagendamento.compex.repository.ICliente;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ClienteService {
+    private ICliente repository;
+
+    public ClienteService(ICliente repository){this.repository = repository;}
+
+    public List<Cliente> listarClientes(){
+        List<Cliente> lista = repository.findAll();
+        return lista;
+    }
+
+    public Cliente criarCliente(Cliente cliente){
+        Cliente clienteNovo = repository.save(cliente);
+        return clienteNovo;
+    }
+
+    public Cliente buscarPorId(Long id) {
+        return repository.findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Cliente não encontrado"));
+    }
+
+    public Cliente editarCliente(Long id, Cliente clienteAtt){
+        Cliente clienteExistente = buscarPorId(id);
+
+        clienteExistente.setNome(clienteAtt.getNome());
+        clienteExistente.setCpf(clienteAtt.getCpf());
+        clienteExistente.setIdade(clienteAtt.getIdade());
+
+        return repository.save(clienteExistente);
+    }
+
+    public void deletarCliente(Long id){
+        Cliente cliente = buscarPorId(id);
+
+        repository.delete(cliente);
+    }
+}
