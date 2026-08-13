3f3cd47ed9560aa623be6bf65266835365e7ccb6
joaozin3661 <joao.ventura@ufpi.edu.br>
2026-08-13 18:51:06 -0300

    Rename ICliente to ClienteRepository

--- src/main/java/com/sistemaagendamento/compex/repository/ClienteRepository.java
+++ src/main/java/com/sistemaagendamento/compex/repository/ClienteRepository.java
@@ -3,5 +3,5 @@ package com.sistemaagendamento.compex.repository;
 import com.sistemaagendamento.compex.model.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ICliente extends JpaRepository <Cliente, Long>{
+public interface ClienteRepository extends JpaRepository <Cliente, Long>{
 }
