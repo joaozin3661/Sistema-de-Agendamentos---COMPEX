53e4785d7490133a788af0c19610e959739ddd04
ivandraws <62605967+ivandraws@users.noreply.github.com>
2026-08-16 17:45:30 -0300

    fix: corrigido URL da API e adicionado id do formulário para leitura do JS

--- src/main/resources/static/scripts/cadastro_horario.js
+++ src/main/resources/static/scripts/cadastro_horario.js
@@ -1,3 +1,34 @@
-const API = 'localhost:8080/api/horarios';
+const API = 'http://localhost:8080/api/horarios';
 const area = document.getElementById('free_horarios')
 
+
+
+
+async function load_horarios() {
+    try {
+        const response = await fetch(API);
+
+        if (!response.ok){
+            throw new Error(`Erro ao carregar horários: ${response.status}`);
+        }
+
+        const horarios = await response.json();
+
+        if (!horarios.lenght)
+        {
+            area.innerHTML = "Sem horários disponíveis";
+        }
+        else
+        {
+            horarios.forEach(horario => {
+                area.appendChild() // inserir função de horário
+            });
+        }
+    } catch (error) {
+        area.textContent = 'Falha na leitura dos dados'
+        console.error("Erro de carregamento: ", error)
+    }
+    
+}
+
+load_horarios()
